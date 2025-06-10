bad7f5da95c065296b6ba9be8540ac4af7ee838c
Willem Jan Glerum <wjglerum@gmail.com>
2025-06-10 16:04:05 +0200

    Enable NullAway

--- src/test/java/nl/wjglerum/GreetingResourceTest.java
+++ src/test/java/nl/wjglerum/GreetingResourceTest.java
@@ -4,17 +4,16 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.containsString;
 
 @QuarkusTest
 class GreetingResourceTest {
     @Test
     void testHelloEndpoint() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(containsString("Hello from Quarkus REST"));
     }
-
 }
